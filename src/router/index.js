c5a5010a57c4fe12c358969ce75149d171069da1
Loan <aubryloan74@gmail.com>
2024-12-20 12:51:50 +0100

    Changed routes + fixed header

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HubView from '@/view/HubView.vue'
-import FluxEditorView from '@/view/FluxEditorView.vue'
-import PreviewView from '@/view/PreviewView.vue'
-
-const routes = [
-  { path: '/', component: HubView },
-  { path: '/flux-editor', component: FluxEditorView },
-  { path: '/preview', component: PreviewView }
-]
+import routes from './routes.js'
 
 const router = createRouter({
   history: createWebHistory(),
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,1 +1,11 @@
-// Définition des routes
+import HubView from '@/view/HubView.vue'
+import FluxEditorView from '@/view/FluxEditorView.vue'
+import PreviewView from '@/view/PreviewView.vue'
+
+const routes = [
+  { path: '/', component: HubView },
+  { path: '/flux-editor', component: FluxEditorView },
+  { path: '/preview', component: PreviewView }
+]
+
+export default routes
